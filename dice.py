e16dcb86becf1ae5ae9c685265556bf9937f1f6c
swaramunoli <swara.munoli@gmail.com>
2025-10-27 22:57:40 +0000

    dice_game

---
+++ dice.py
@@ -0,0 +1,14 @@
+#!/bin/env python 
+
+import random
+
+permission = input("yes or no. Do you want to roll the dice? If 5, you win. ")
+
+if permission == "yes":
+    dice = random.randrange(1,6)
+    print(dice)
+
+if dice ==5:
+    print("YOU WON !!!")
+else:
+    print("you lost :(")
